149216bcfabe2714588c98327b8017f1185c53cc
Andrehlb <andreh.72@hotmail.com>
2025-05-02 18:49:07 -0300

    feat: implement eventoService functions using EventoModel for PostgreSQL data manipulation

--- src/evento/eventoService.js
+++ src/evento/eventoService.js
@@ -1,3 +1,38 @@
+// 2o formato src/evento/eventoService.js vai usar as funções do src/evento/eventoModel.js com PostGreSQ, no lugar da versão anterior para manipular os dados dos eventos.
+const EventoModel = require('./eventoModel');
+
+async function criar(dto) {
+  return await EventoModel.criar(dto);
+}
+
+async function listar() {
+  return await EventoModel.listar();
+}
+
+async function buscar(id) {
+  return await EventoModel.buscar(id);
+}
+
+async function atualizar(id, dto) {
+  return await EventoModel.atualizar(id, dto);
+}
+
+async function remover(id) {
+  return await EventoModel.remover(id);
+}
+
+module.exports = {
+  criar,
+  listar,
+  buscar,
+  atualizar,
+  remover
+};
+
+
+
+// 1o formato src/evento/eventoService.js Acessar endpoint: http://localhost:3000/eventos
+/*
 const Evento = require('./eventoModel');
 
 let eventos = [];
@@ -55,3 +90,4 @@ module.exports = {
   atualizar,
   remover
 };
+*/
