c1a3a5f1d79f6f4009a4ff4288144086c749d7c6
Andrehlb <andreh.72@hotmail.com>
2025-05-02 20:36:53 -0300

    feat: add hora and descricao fields to EventoDTO constructor

--- src/evento/eventoDTO.js
+++ src/evento/eventoDTO.js
@@ -3,7 +3,9 @@ class EventoDTO {
       this.nome = nome;
       this.tipo = tipo;
       this.data = data;
+      this.hora = hora;
       this.local = local;
+      this.descricao = descricao;
       this.disponibilidade = disponibilidade;
     }
   }
